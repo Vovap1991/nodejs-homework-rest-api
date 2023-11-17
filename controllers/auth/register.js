d0cd784deb4d32be3ef15fd37aab9778707f917c
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-11-17 18:40:01 +0200

    add password hashing with bcrypt

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../../models/user");
 const { validateRegistration } = require("../../validation/auth");
 
@@ -17,7 +19,9 @@ async function register(req, res, next) {
       return res.status(409).send({ message: "Email in use" });
     }
 
-    await User.create({ email, password });
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    await User.create({ email, password: passwordHash });
     res.status(201).send({
       user: {
         email,
