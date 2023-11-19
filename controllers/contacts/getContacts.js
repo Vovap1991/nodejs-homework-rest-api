e89e6da43bacb228d992d5f73a3acc09a6c7dd0b
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-11-19 19:31:01 +0200

    pagination

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,7 +2,13 @@ const Contact = require("../../models/contact");
 
 async function getContacts(req, res, next) {
   try {
-    const contacts = await Contact.find({ owner: req.user.id }).exec();
+    const { page = 1, limit = 20 } = req.query;
+    const skip = (page - 1) * limit;
+
+    const contacts = await Contact.find({ owner: req.user.id }, " ", {
+      skip,
+      limit: Number(limit),
+    }).exec();
     res.status(200).json(contacts);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
